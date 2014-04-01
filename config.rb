533bf08919b9b0b5241cb0e536a02845dc18d246
mryabko <dayteporabotat@ukr.net>
2014-04-01 18:56:36 +0300

    first commit

---
+++ config.rb
@@ -0,0 +1,7 @@
+http_path = "/"
+css_dir = "gen-css"
+sass_dir = "scss"
+images_dir = "images"
+generated_images_dir = "compiled_sprites"
+sprite_load_path = "skin/frontend/pugu/default/images/sprites"
+output_style = :compressed
